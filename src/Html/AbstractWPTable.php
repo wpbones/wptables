c5f1fbc9e90984f4c082525080605dd8d36b9ada
John Fazioli <john.fazioli@namecheap.com>
2016-12-28 18:00:41 +0100

    complete with search box and bulk actions

--- src/Html/AbstractWPTable.php
+++ src/Html/AbstractWPTable.php
@@ -14,34 +14,106 @@ if ( ! class_exists( 'WP_List_Table' ) ) {
 
 abstract class AbstractWPTable extends \WP_List_Table
 {
-  protected $_items = [];
-
   protected $singular;
 
   protected $plural;
-
-  protected $name;
-
-  protected $ajax = false;
 
   protected $itemPerPageName;
 
   protected $defaultItemPerPage = 5;
 
-  protected $checkBoxes = true;
-
   protected $columns = [];
 
   protected $screenOptionsLabel = '';
 
+  protected $searchBoxButtonLabel = '';
+
+  protected $searchBoxInputId = '';
+
+  protected $addNewLabel = '';
+
+  /**
+   * The name of this list table.
+   *
+   * @var string
+   */
+  protected $name = '';
+
+  /**
+   * Display a well format title above the list table.
+   *
+   * @var string
+   */
+  protected $title = '';
+
+  /**
+   * Set TRUE to enable the checkboxes column.
+   *
+   * @var bool
+   */
+  protected $checkBoxes = true;
+
+  /**
+   * Set TRUE to enable Ajax pagination.
+   *
+   * @var bool
+   */
+  protected $ajax = false;
+
+  /**
+   * Set to TRUE to display the search box in the top right of the list table.
+   * When true, you may override `getSearchBoxButtonLabelAttribute()` and
+   * `getSearchBoxInputIdAttribute()` attribute methods. Also, you can use the
+   * related properties: `searchBoxButtonLabel` and `searchBoxInputId`.
+   *
+   * @var bool
+   */
+  protected $searchBox = false;
+
+  /**
+   * List of the input type hidden inject into the form.
+   *
+   * @var array
+   */
+  protected $inputsHidden = [
+    'page',
+    'post_type',
+    'orderby',
+    'order',
+    '_action_result',
+  ];
+
+  /**
+   * You can set these properties after an action.
+   *
+   * @var string
+   */
+  protected $successMessage = '';
+
+  protected $errorMessage = '';
+
+  protected $infoMessage = '';
+
+  protected $warningMessage = '';
+
+  /**
+   * AbstractWPTable constructor.
+   *
+   * You can use the `boot()` method instead override the constructor.
+   *
+   * @param array $args Optional. The standar WP List Table args.
+   */
   public function __construct( $args = [] )
   {
+    if ( method_exists( $this, 'boot' ) ) {
+      $this->boot();
+    }
+
     $args = wp_parse_args( $args,
                            [
                              'plural'   => $this->getPluralAttribute(),
                              'singular' => $this->getSingularAttribute(),
                              'ajax'     => $this->ajax,
-                             'screen'   => $this->getIdAttribute(),
                            ]
     );
 
@@ -76,11 +148,28 @@ abstract class AbstractWPTable extends \WP_List_Table
     }
   }
 
+  /**
+   * Return an instance of class. This is a useful method.
+   * Anyway, any method or miss method will return an instance of class.
+   *
+   * @param array $args
+   *
+   * @return static
+   */
   public static function init( $args = [] )
   {
     return new static( $args );
   }
 
+  /**
+   * Used to set attribute by a static call.
+   * Anyway, if the static method is not found, an instance of class is returned.
+   *
+   * @param $name
+   * @param $arguments
+   *
+   * @return static
+   */
   public static function __callStatic( $name, $arguments )
   {
     $instance = new static();
@@ -88,7 +177,12 @@ abstract class AbstractWPTable extends \WP_List_Table
     $method = "set" . Str::studly( $name ) . 'Attribute';
 
     if ( method_exists( $instance, $method ) ) {
-      $instance->{$method}( $arguments[ 0 ] );
+      if ( empty( $arguments ) ) {
+        $instance->{$method}();
+      }
+      else {
+        $instance->{$method}( $arguments[ 0 ] );
+      }
     }
 
     return $instance;
@@ -99,11 +193,31 @@ abstract class AbstractWPTable extends \WP_List_Table
     $method = "set" . Str::studly( $name ) . 'Attribute';
 
     if ( method_exists( $this, $method ) ) {
-      $this->{$method}( $arguments[ 0 ] );
+      if ( empty( $arguments ) ) {
+        $this->{$method}();
+      }
+      else {
+        $this->{$method}( $arguments[ 0 ] );
+      }
     }
 
     return $this;
   }
+
+  public function __toString()
+  {
+    return $this->html();
+  }
+
+  /*
+  |--------------------------------------------------------------------------
+  | Get/Set attributes
+  |--------------------------------------------------------------------------
+  |
+  | Here you'll find the get/set for dynamic attributes. These can be used
+  | either for override a subclass or like fluent instance.
+  |
+  */
 
   public function setRegisterScreenOptionAttribute()
   {
@@ -165,12 +279,12 @@ abstract class AbstractWPTable extends \WP_List_Table
 
   public function getItems( $args = [] )
   {
-    return $this->_items;
+    return $this->items;
   }
 
   public function setItems( $value )
   {
-    $this->_items = $value;
+    $this->items = $value;
 
     return $this;
   }
@@ -211,6 +325,30 @@ abstract class AbstractWPTable extends \WP_List_Table
     return $this;
   }
 
+  public function getSearchBoxButtonLabelAttribute()
+  {
+    return empty( $this->searchBoxButtonLabel ) ? __( 'Search' ) : $this->searchBoxButtonLabel;
+  }
+
+  public function setSearchBoxButtonLabelAttribute( $value )
+  {
+    $this->searchBoxButtonLabel = $value;
+
+    return $this;
+  }
+
+  public function getSearchBoxInputIdAttribute()
+  {
+    return empty( $this->searchBoxInputId ) ? 'search_id' : $this->searchBoxInputId;
+  }
+
+  public function setSearchBoxInputIdAttribute( $value )
+  {
+    $this->searchBoxInputId = $value;
+
+    return $this;
+  }
+
   /**
    * Associative array with the list of columns.
    *
@@ -226,6 +364,243 @@ abstract class AbstractWPTable extends \WP_List_Table
     $this->columns = $value;
 
     return $this;
+  }
+
+  public function getAddNewLabelAttribute()
+  {
+    return empty( $this->addNewLabel ) ? __( 'Add New' ) : $this->addNewLabel;
+  }
+
+  public function getTitleAttribute()
+  {
+    return $this->title;
+  }
+
+  public function setTitleAttribute( $value )
+  {
+    $this->title = $value;
+
+    return $this;
+  }
+
+  public function getAddNewUri()
+  {
+    return false;
+  }
+
+  /**
+   * You have override this method in order to display the table views.
+   * This will return an associative array look like:
+   *
+   *    return array(
+   *      'all'     => __( 'All' ),
+   *      'publish' => __( 'Publish' ),
+   *    );
+   *
+   * @return array
+   */
+  public function getViews()
+  {
+    return [];
+  }
+
+  /**
+   * Usually, you have to override this method when you are provide some view.
+   * This will return the current view. For example, you may to check the $_REQUEST[] global var.
+   *
+   * @return string
+   */
+  public function getCurrentView()
+  {
+    return 'all';
+  }
+
+  /**
+   * You have to override this method to provide the bulk actions drop down menu.
+   * Will return an associative array look like:
+   *
+   *    return [
+   *      'delete' => __( 'Delete' )
+   *    ];
+   *
+   * Also, you can use $view input parameter to figure out which view is displayed.
+   *
+   * @param string $view The current view.
+   *
+   * @return array
+   */
+  public function getBulkActionsForView( $view )
+  {
+    return [];
+  }
+
+  /**
+   * Return the search value of input;
+   *
+   * @return bool|string
+   */
+  public function getSearchValue()
+  {
+    if ( $this->searchBox ) {
+      if ( isset( $_REQUEST[ 's' ] ) && ! empty( $_REQUEST[ 's' ] ) ) {
+        return $_REQUEST[ 's' ];
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Will call a method when find a bulk action.
+   * For example, if the acrion is "keep_on_trash" then you will have `processBulkActionKeepOnTrash()` method.
+   *
+   * @return bool
+   */
+  protected function processBulkActions()
+  {
+    $action = $this->getCurrentBulkAction();
+
+    if ( $action ) {
+
+      $method = 'processBulkAction' . Str::studly( $action );
+
+      if ( method_exists( $this, $method ) ) {
+
+        $items = isset( $_REQUEST[ $this->getIdAttribute() ] ) ? $_REQUEST[ $this->getIdAttribute() ] : [];
+
+        $this->{$method}( $items );
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Return the current bulk action. If no bulk actions are set will return FALSE.
+   *
+   * @return bool
+   */
+  protected function getCurrentBulkAction()
+  {
+    if ( isset( $_REQUEST[ 'action' ] ) && -1 != $_REQUEST[ 'action' ] ) {
+      return $_REQUEST[ 'action' ];
+    }
+
+    if ( isset( $_REQUEST[ 'action2' ] ) && -1 != $_REQUEST[ 'action2' ] ) {
+      return $_REQUEST[ 'action2' ];
+    }
+
+    return false;
+  }
+
+  /**
+   * Usually, you shouldn't override this method.
+   * Instead use `getViewCount{Viewkey}`.
+   *
+   * This will return the number of items for selected view.
+   *
+   * @param string $view The view key.
+   *
+   * @return int
+   */
+  protected function getCountForView( $view )
+  {
+    if ( $view === 'all' ) {
+      return count( $this->items );
+    }
+
+    $method = 'getViewCount' . Str::studly( $view );
+
+    if ( method_exists( $this, $method ) ) {
+      return $this->{$method}();
+    }
+
+    return 0;
+  }
+
+  /**
+   * Usually, you shouldn't override this method.
+   * Instead use `getViewQueryArg{Viewkey}`.
+   *
+   * This will return an empty array for `all` key or your query args like an associative
+   * array, look like:
+   *
+   *  [
+   *    'ingredients' => 'cream'
+   *  ]
+   *
+   * @param string $view The view key.
+   *
+   * @return array
+   */
+  protected function getQueryArgsForView( $view )
+  {
+    if ( $view === 'all' ) {
+      return [];
+    }
+
+    $method = 'getViewQueryArg' . Str::studly( $view );
+
+    if ( method_exists( $this, $method ) ) {
+      return $this->{$method}();
+    }
+
+    return [];
+
+  }
+
+  /*
+  |--------------------------------------------------------------------------
+  | Original WP List Table
+  |--------------------------------------------------------------------------
+  |
+  | Here you'll find the subclassed method from WP List Table
+  |
+  */
+
+  public function get_bulk_actions()
+  {
+    $currentView = $this->getCurrentView();
+
+    return $this->getBulkActionsForView( $currentView );
+  }
+
+  public function get_views()
+  {
+    $views = [];
+
+    $currentView = $this->getCurrentView();
+
+    $allViews = $this->getViews();
+
+    if ( ! empty( $allViews ) ) {
+
+      if ( ! in_array( 'all', array_keys( $allViews ) ) ) {
+        $allViews = array_merge( [ 'all' => __( 'All' ) ], $allViews );
+      }
+
+      foreach ( $allViews as $key => $label ) {
+
+        $count = $this->getCountForView( $key );
+
+        if ( ! empty( $count ) ) {
+          $class = ( $key == $currentView ) ? 'current' : '';
+          $href  = $this->getUriForView( $key );
+
+          $views[ $key ] = sprintf( '<a class="%s" href="%s">%s <span class="count">(%s)</span></a>',
+                                    $class,
+                                    $href,
+                                    $label,
+                                    $count
+          );
+        }
+      }
+
+    }
+
+    return $views;
   }
 
   public function get_columns()
@@ -289,15 +664,14 @@ abstract class AbstractWPTable extends \WP_List_Table
 
   /**
    * Handles data query and filter, sorting, and pagination.
+   * Will return TRUE if the items are prepared. Instead, will return FALSE when any bulk actions have been performed.
    */
   public function prepare_items()
   {
     $this->_column_headers = $this->get_column_info();
 
-    // Process bulk action
-    if ( method_exists( $this, 'processBulkAction' ) ) {
-      call_user_func( [ $this, 'processBulkAction' ] );
-    }
+    // Process bulk actions
+    $this->processBulkActions();
 
     $perPage = $this->get_items_per_page( $this->getItemPerPageNameAttribute(), $this->getDefaultItemPerPageAttribute() );
 
@@ -344,24 +718,31 @@ abstract class AbstractWPTable extends \WP_List_Table
       ]
     );
 
+    return true;
+
   }
 
   /**
    * Message to be displayed when there are no items
    *
-   * @since  3.1.0
-   * @access public
    */
   public function no_items()
   {
-    if ( method_exists( $this, 'getNoItemsMessage' ) ) {
-      call_user_func( [ $this, 'getNoItemsMessage' ] );
-    }
-    else {
-      parent::no_items();
+    // Default message
+    echo '<p style="text-align:center">';
+    echo '<strong>';
+    printf( __( 'No %s found.' ), $this->name );
+    echo '</strong>';
 
+
+    if ( $this->getSearchValue() ) {
+
+      echo '<br/>';
+
+      _e( 'Please, check again your search parameters.' );
     }
 
+    echo '</p>';
   }
 
   /**
@@ -377,10 +758,162 @@ abstract class AbstractWPTable extends \WP_List_Table
     return $item[ $columnName ];
   }
 
+  public function html()
+  {
+    ob_start();
+
+    if ( ! empty( $this->title ) ) {
+      $this->displayTitle();
+    }
+
+    $this->prepare_items();
+
+    $this->views();
+
+    ?>
+    <form id="<?php echo $this->getIdAttribute() ?>"
+          class="<?php echo $this->getNameAttribute() ?>"
+          method="get"><?php
+
+    if ( $this->searchBox ) {
+      $this->search_box( $this->getSearchBoxButtonLabelAttribute(), $this->getSearchBoxInputIdAttribute() );
+    }
+
+    foreach ( $this->inputsHidden as $name ) {
+      if ( isset( $_REQUEST[ $name ] ) ) {
+        $value = is_array( $_REQUEST[ $name ] ) ? esc_attr( $_REQUEST[ $name ][ 0 ] ) : esc_attr( $_REQUEST[ $name ] );
+        echo "<input type=\"hidden\" name=\"{$name}\" value=\"{$value}\" />";
+      }
+    }
+
+    parent::display();
+
+    ?></form><?php
+
+    if ( ! empty( $this->successMessage ) ) {
+      ?>
+      <div class="notice notice-success is-dismissible">
+        <p><?php echo $this->successMessage ?></p>
+      </div>
+      <?php
+    }
+
+    if ( ! empty( $this->errorMessage ) ) {
+      ?>
+      <div class="notice notice-error is-dismissible">
+        <p><?php echo $this->errorMessage ?></p>
+      </div>
+      <?php
+    }
+
+    if ( ! empty( $this->warningMessage ) ) {
+      ?>
+      <div class="notice notice-warning is-dismissible">
+        <p><?php echo $this->warningMessage ?></p>
+      </div>
+      <?php
+    }
+
+    if ( ! empty( $this->infoMessage ) ) {
+      ?>
+      <div class="notice notice-info is-dismissible">
+        <p><?php echo $this->infoMessage ?></p>
+      </div>
+      <?php
+    }
+
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    return $content;
+  }
+
   public function display()
   {
-    $this->prepare_items();
-    parent::display();
+    echo $this->html();
+  }
+
+  /*
+  |--------------------------------------------------------------------------
+  | Private methods
+  |--------------------------------------------------------------------------
+  |
+  | Here you'll find the private methods.
+  |
+  */
+
+  private function displayTitle()
+  {
+    ?>
+    <h1 class="wp-heading-inline">
+      <?php echo $this->title ?>
+    </h1>
+
+    <?php if ( ! empty( $this->getAddNewUri() ) && ! empty( $this->getAddNewLabelAttribute() ) ) : ?>
+
+    <a href="#"
+       class="page-title-action">
+      <?php echo $this->getAddNewLabelAttribute() ?>
+    </a>
+
+  <?php endif; ?>
+
+    <hr class="wp-header-end">
+
+    <h2 class="screen-reader-text">
+      <?php echo $this->title ?>
+    </h2>
+    <?php
+  }
+
+  private function isAjax()
+  {
+    if ( defined( 'DOING_AJAX' ) ) {
+      return true;
+    }
+    if ( isset( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] ) && strtolower( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] ) == 'xmlhttprequest'
+    ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  private function getReferer()
+  {
+    $referer = $this->isAjax() ? $_SERVER[ 'HTTP_REFERER' ] : $_SERVER[ 'REQUEST_URI' ];
+
+    $remove = [
+      '_wp_http_referer',
+      'action',
+      'action2',
+      '_wpnonce',
+      $this->_args[ 'singular' ],
+      $this->_args[ 'plural' ],
+      'paged',
+      '_action_result',
+    ];
+
+    foreach ( array_keys( $this->getViews() ) as $view ) {
+      $remove = array_filter( array_merge( $remove, array_keys( $this->getQueryArgsForView( $view ) ) ) );
+    }
+
+    $referer = remove_query_arg( $remove, $referer );
+
+    return $referer;
+
+  }
+
+  private function getUriForView( $view )
+  {
+    $args = $this->getQueryArgsForView( $view );
+
+    if ( $args ) {
+      return add_query_arg( $this->getQueryArgsForView( $view ), $this->getReferer() );
+    }
+
+    return $this->getReferer();
   }
 
 
